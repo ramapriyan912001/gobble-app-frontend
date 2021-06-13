c2d121642d9364a449789c27aacc015cb9dd3835
Ramapriyan Srivatsan <ramapriyan912001@gmail.com>
2021-06-13 12:13:07 +0800

    Change

--- src/dispatchers/profileDispatchers.js
+++
@@ -1,7 +0,0 @@
-import {useDispatch} from 'react-redux'
-import {updateProfile} from '../actions/profile'
-
-export const submitProfile = (profile) => {
-    const dispatch = useDispatch();
-    dispatch(updateProfile(profile))
-}
