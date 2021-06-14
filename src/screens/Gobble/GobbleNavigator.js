3499fa873307a30ad30653330cb648835dbe3045
Ramapriyan Srivatsan <ramapriyan912001@gmail.com>
2021-06-14 10:32:04 +0800

    change

--- src/screens/Gobble/GobbleNavigator.js
+++ src/screens/Gobble/GobbleNavigator.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import {View, Text} from 'react-native'
-import {containerStyles} from '../../styles/LoginStyles'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { fetchUser, clearData } from '../../actions/index'
