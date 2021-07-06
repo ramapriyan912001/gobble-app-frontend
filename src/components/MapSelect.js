bf0ff7d8444abfc70ca286632be6a6fe4819158b
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-07-06 10:35:34 +0800

    changes

--- src/components/MapSelect.js
+++ src/components/MapSelect.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
 import { View, Text } from 'react-native'
 import { EMPTY_AVATAR } from '../constants/objects'
 import { Ionicons } from '@expo/vector-icons'
 
 export default function MapSelect(props) {
+
+    const [lat, setLat] = useState(0)
+    const [long, setLong] = useState(0)
+    const [edited, setEdited] = useState(false);
+
+    useEffect(() => {
+        // console.log(props)
+        // console.log()
+        // if(props.route.params && !edit) {
+        //     setLat(props.route.params.lat)
+        //     setLong(props.route.params.long)
+        // }
+    }, [lat, long])
+
     return (
         <View
          style={{
@@ -28,7 +42,7 @@ export default function MapSelect(props) {
                 }}
             >
             <Marker
-            coordinate={{latitude: 37.78825, longitude: -122.4324}}>
+            coordinate={{latitude: lat, longitude: long}}>
                 <Ionicons name='fast-food' color={'#008a49'} size={28}></Ionicons>
             </Marker>
             </MapView>
