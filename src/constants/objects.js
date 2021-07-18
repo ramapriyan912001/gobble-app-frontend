961a89e78946642dc006d008543c8929bb2c91ea
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-07-18 18:31:15 +0800

    delete vegan and add strictly vegetarian and strictly halal

--- src/constants/objects.js
+++ src/constants/objects.js
@@ -1,10 +1,3 @@
-export const DIETARY_ARRAYS = {//was Bug
-    "Vegetarian": ["Vegetarian", 'Vegan', "Any"],
-    "Halal": ["Halal", "Any", "Vegetarian", "Vegan"],
-    "Vegan": ["Vegan", "Any"],
-    "Any": ["VEGAN", "VEGETARIAN", "HALAL", "Any"],
-}
-
 export const FOOD_IMAGES_URIs = {
     "Indian": 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/icons%2FIndian.jpg?alt=media&token=f400ee28-730a-43ca-8097-9959ae719e3b',
     "Western": 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/icons%2FWestern.jpg?alt=media&token=6f3d75a1-3441-42b1-a8bd-86f8058bc30c',
@@ -29,9 +22,9 @@ export const INDUSTRY_CODES = {
 }
 
 export const CUISINES = ['Western', 'Indian', 'Asian', 'Food Court', 'Any']
-export const REASONS = ['Spam', 'Inappropriate', 'Harassment', 'Sexual Harassment', 'Others']
+export const REASONS = ['Spam', 'Unprofessional', 'Harassment', 'Sexual Harassment', 'Others']
 
-export const DIETS = ['Halal', 'Vegetarian', 'Vegan', 'Any']
+export const DIETS = ['Vegetarian', 'Strictly Vegetarian', 'Halal', 'Strictly Halal', 'Any']
 
 export const EMPTY_AVATAR = 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/avatar%2Fempty_avatar.png?alt=media&token=c36c29b3-d90b-481f-a9d9-24bc73619ddc'
 export const GOBBLE_IMAGE_URI = 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/icons%2Fadaptive-icon.png?alt=media&token=b3b418d2-6c37-4f0d-bcc7-5fd94abc615e'
