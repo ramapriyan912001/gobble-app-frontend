4dc736c235e27ef8ed7ba57cc2f3e5292099446a
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-07-18 18:19:04 +0800

    ANY to Any

--- src/constants/objects.js
+++ src/constants/objects.js
@@ -1,16 +1,15 @@
 export const DIETARY_ARRAYS = {//was Bug
-    "Vegetarian": ["Vegetarian", 'Vegan', "ANY"],
-    "Halal": ["Halal", "ANY", "Vegetarian", "Vegan"],
-    "Vegan": ["Vegan", "ANY"],
-    "ANY": ["VEGAN", "VEGETARIAN", "HALAL", "ANY"],
-    "Any": ["VEGAN", "VEGETARIAN", "HALAL", "ANY"]
+    "Vegetarian": ["Vegetarian", 'Vegan', "Any"],
+    "Halal": ["Halal", "Any", "Vegetarian", "Vegan"],
+    "Vegan": ["Vegan", "Any"],
+    "Any": ["VEGAN", "VEGETARIAN", "HALAL", "Any"],
 }
 
 export const FOOD_IMAGES_URIs = {
     "Indian": 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/icons%2FIndian.jpg?alt=media&token=f400ee28-730a-43ca-8097-9959ae719e3b',
     "Western": 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/icons%2FWestern.jpg?alt=media&token=6f3d75a1-3441-42b1-a8bd-86f8058bc30c',
     'Asian': 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/icons%2FAsian.jpg?alt=media&token=42a2bb66-5128-40b3-bbdf-90a3a08f0050',
-    'ANY': 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/icons%2FAnyCuisine.jpg?alt=media&token=a81028e1-b1f7-466d-a7a6-426538d9d917'
+    'Any': 'https://firebasestorage.googleapis.com/v0/b/gobble-b3dfa.appspot.com/o/icons%2FAnyCuisine.jpg?alt=media&token=a81028e1-b1f7-466d-a7a6-426538d9d917'
 }
 
 export const INDUSTRY_CODES = {
