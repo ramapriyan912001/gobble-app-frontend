6ace13d5aa89ab4a8eef769911a4c79003bafd5c
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-07-22 12:32:22 +0800

    some test templates

---
+++ src/screens/__tests__/GobbleSelect2.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import GobbleSelect2 from '../Gobble/GobbleSelect2';
+import renderer from 'react-test-renderer';
+import {jest, expect} from '@jest/globals';
---
+++ src/screens/__tests__/MealPreferences.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import MealPreferences from '../MealPreferences';
+import renderer from 'react-test-renderer';
+import {jest, expect} from '@jest/globals';
